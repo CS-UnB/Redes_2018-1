d1465b1f497099155b491043c4db4af25ebe754e
Otto K. von Sperling <ottok92@gmail.com>
2018-04-29 16:27:27 -0300

    POST & GET object working
    not tested in network yet

--- LAB2/client_socket_prog.py
+++ LAB2/client_socket_prog.py
@@ -1,5 +1,6 @@
 # CONTINUE from testing GET and developing POST objects
 import socket
+import sys, getopt
 
 class MyClient:
 ## START __METHODS__ #########################################################
@@ -23,6 +24,9 @@ class MyClient:
 		elif response[:3] == '204':
 			print ('Object found. Receiving it...\n')
 			self.receive_object(object)
+		#elif response[:3] == '104':
+		#	print ('Getting ready to post object to server')
+		#	self.send_object(object)
 
 	def get(self, host, target, port=80):
 		request_header = 'GET / HTTP/1.0\r\nHost:'+ host +'\r\nTarget:'+ target +'\r\nPort:'+ str(port) +'\r\n\r\n'
@@ -31,7 +35,13 @@ class MyClient:
 		self.wait_reply(target)
 		#self.sock.close()
 		
-	def send_file(self, file_name):
+	def post(self, host, file_name, port=80):
+		request_header = 'POST / HTTP/1.0\r\nHost:'+ host +'\r\nObject:'+ file_name +'\r\nPort:'+ str(port) +'\r\n\r\n'
+		self.sock.send(request_header.encode('UTF-8'))
+		self.send_object(file_name)
+		#self.wait_reply(file_name)
+
+	def send_object(self, file_name):
 		print ('Sending object\n')
 		with open(file_name, 'rb') as f:
 			self.sock.sendfile(f, 0)
@@ -39,7 +49,7 @@ class MyClient:
 		self.sock.close()
 
 	def receive_object(self, file_name):
-		with open(str('./client_'+ file_name[2:]), 'wb') as f:
+		with open(str('./client_'+ file_name), 'wb') as f:
 			data = self.sock.recv(1024)
 			while data:
 				print ('\nReceiving object...\n')
@@ -50,10 +60,36 @@ class MyClient:
 
 ## END __METHODS__ ############################################################
 
-def Main():
+def Main(argv=None):
 	client = MyClient()
 	client.connect_to_host()
-	client.get(str(client.host), './txt1.txt', )
+
+	try:
+		opts, args = getopt.getopt(argv, "h", ["get=", "post=", "head="])
+	except getopt.GetoptError:
+		print ('http_client.py --get host/object_path || --post host/object_path || --head ?')
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print ('\t\tHELP MENU\n')
+			print ('http_client.py --get host/object_path || --post host/object_path || --head ?')
+			sys.exit()
+		elif opt == '--get':
+			host, obj = arg.split('/', 1)
+			client.get(host, obj, )
+			# draw menu
+		elif opt == '--post':
+			host, obj = arg.split('/', 1)
+			client.post(host, obj, )
+			#draw menu
+		elif opt == '--head':
+			print (arg)
+			#draw menu
+			
 
 if __name__ == '__main__':
-	Main()
+	if len(sys.argv) > 1:
+		Main(sys.argv[1:])
+	else:
+		print ('Command formating incorrect. Not enough parameters.\nTry -h for help')
+		# error msg
