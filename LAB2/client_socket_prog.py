48397e5e135051f218b2dfee304c8793c1f31be7
Otto K. von Sperling <ottok92@gmail.com>
2018-04-29 13:29:23 -0300

    GET done, developing POST

--- LAB2/client_socket_prog.py
+++ LAB2/client_socket_prog.py
@@ -1,7 +1,8 @@
+# CONTINUE from testing GET and developing POST objects
 import socket
 
 class MyClient:
-
+## START __METHODS__ #########################################################
 	def __init__(self, sock=None):
 		if sock is None:
 			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,45 +10,50 @@ class MyClient:
 			self.sock = sock
 
 	def connect_to_host(self, host=socket.gethostname(), port=80):
+		self.host = host
 		self.sock.connect((host, port))
 
-	def get(self, host, port=80):
-		request_header = 'GET / HTTP/1.0\r\nHost:' + host + '\r\nPort:' + str(port) + '\r\n\r\n'
-		self.sock.send(request_header.encode('UTF-8'))
+	def wait_reply(self, object):
+		response = self.sock.recv(1024).decode()
 
-	def receive(self):
-		response = ''
-		while True:
-		    recv = self.sock.recv(1024)
-		    if not recv:
-		        break
-		    response += recv.decode()
-		print (response)
-	
-	def send_file(self, file_name='name.type'):
-		f = open(file_name, 'rb')
-		l = f.read(1024)
-		while(l):
-			self.sock.send(l)
-			l = f.read(1024)
-		f.close()
-	def receive_file(self, client, file_name='name.type'):
-      f = open(file_name, 'wb')
-      while (True):
-         l = client.sock.recv(1024)
-         while (l):
-            f.write(l)
-            l = client.sock.recv(1024)
-      f.close()
-      
-	def close(self):
+		if response[:3] == '400':
+			print ('Incorrect host reached. Please, retry.\n')
+		elif response[:3] == '404':
+			print ('File not found in host\n')
+		elif response[:3] == '204':
+			print ('Object found. Receiving it...\n')
+			self.receive_object(object)
+
+	def get(self, host, target, port=80):
+		request_header = 'GET / HTTP/1.0\r\nHost:'+ host +'\r\nTarget:'+ target +'\r\nPort:'+ str(port) +'\r\n\r\n'
+		self.sock.send(request_header.encode('UTF-8'))
+		self.sock.shutdown(socket.SHUT_WR) # signals it has finished sending
+		self.wait_reply(target)
+		#self.sock.close()
+		
+	def send_file(self, file_name):
+		print ('Sending object\n')
+		with open(file_name, 'rb') as f:
+			self.sock.sendfile(f, 0)
+		print ('Done sending object\n\n')
 		self.sock.close()
 
+	def receive_object(self, file_name):
+		with open(str('./client_'+ file_name[2:]), 'wb') as f:
+			data = self.sock.recv(1024)
+			while data:
+				print ('\nReceiving object...\n')
+				f.write(data)
+				data = self.sock.recv(1024)
+			self.sock.close()
+			print ('Done receiving object.\n')
+
+## END __METHODS__ ############################################################
+
 def Main():
-	client_socket = MyClient()
-	client_socket.connect_to_host()
-	#client_socket.receive()
-	client_socket.get(socket.gethostname(), )
+	client = MyClient()
+	client.connect_to_host()
+	client.get(str(client.host), './txt1.txt', )
 
 if __name__ == '__main__':
 	Main()
--- LAB2/server_socket_prog.py
+++ LAB2/server_socket_prog.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3           # This is server.py file
+# CONTINUE from testing GET and developing POST objects
 import socket 
 from threading import *
-import sys       
+import os.path  
 
 class client_thread(Thread):
 
@@ -12,7 +13,7 @@ class client_thread(Thread):
       self.start()
 
 class MyServer:
-
+## START __METHODS__ #########################################################
    def __init__(self, sock=None):
       if sock is None:
          self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,31 +35,63 @@ class MyServer:
       print ("Got a connection from %s" % str(addr))
       return client
 
-   def close_socket(self):
-      self.sock.close()
-
    def respond(self, client, msg=None):
       if msg is None:
          msg = 'Thank you for connecting\r\n'
       client.sock.send(msg.encode('UTF-8'))
 
-   def receive(self):
-      response = ''
-      while (True):
-          recv = self.sock.recv(1024)
-          if not recv:
-              break
-          response += recv.decode()
-      print (response)
+   def handle_get(self, client, target):
+      if os.path.isfile(target):
+         print(target +' object found\n')
+         self.respond(client, str('204\r\nRequested Object '+ target +' found\r\n'))
+         self.send_object(client, target)
+      else:
+         print (target +' NOT FOUND')
+         self.respond(client, str('404\r\nRequested Object '+ target +' not found\r\n'))
 
-   def receive_file(self, client, file_name='name.type'):
-      f = open(file_name, 'wb')
+   def receive_request(self, client):
+      request = ''
       while (True):
-         l = client.sock.recv(1024)
-         while (l):
-            f.write(l)
-            l = client.sock.recv(1024)
-      f.close()
+          recv = client.sock.recv(1024)
+          if recv == b'':
+              break
+          request += recv.decode()
+
+      method, host, target, port = request.split("\r\n", 3)
+      # cleaning meta-data
+      method = method[:3]
+      host = host[5:] 
+      target = target[7:]
+      port = port[5:]
+      
+      # resolving request 
+      if host != socket.gethostname():
+         print ('Requested host '+ host +' does not match localhost '+ socket.gethostname() +'\n')
+         self.respond(client, '400\r\nIncorrect HOST reached!\r\n')
+      else:
+         if method == 'GET':
+            print ('Object '+ target +' requested by client '+ str(client.addr) +'\n')
+            self.handle_get(client, target)
+            
+
+
+   def receive_object(self, client, file_name):
+      with open(str('./server_'+ file_name[2:]), 'wb') as f:
+         data = client.sock.recv(1024)
+         while data:
+            print ('\nReceiving object...\n')
+            f.write(data)
+            data = client.sock.recv(1024)
+         client.sock.close()
+         print ('Done receiving object.\n')
+
+   def send_object(self, client, file_name):
+      print ('Sending object\n')
+      with open(file_name, 'rb') as f:
+         client.sock.sendfile(f, 0)
+      print ('Done sending object\n\n')
+      client.sock.close()
+## END __METHODS__ ###########################################################
 
 def Main():
    server = MyServer()
@@ -66,11 +99,9 @@ def Main():
    server.listen_to(3)
    for i in range(5):
          # receive connection from clients
-         client = server.accept_inbound_connection()
-         #server.respond(client, )
-         data = client.sock.recv(1024).decode()
-         print (data)
-         client.sock.close()
+         client_thread = server.accept_inbound_connection()
+         server.receive_request(client_thread)
+         # close client socket ?
    server.sock.close()
 
 if __name__ == '__main__':
